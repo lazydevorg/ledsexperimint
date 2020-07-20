b2d5544f7cdcf2378e444ccd65925578f68cca3e
Fabio Volpe <volpefabio@gmail.com>
2020-07-21 00:08:31 +0100

    SWO configuration

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate panic_halt; // panic handler
 
+use cortex_m::iprintln;
 use cortex_m_rt::entry;
 use f3::{
     hal::{delay::Delay, prelude},
@@ -17,27 +18,11 @@ use f3::hal::{prelude::*, stm32f30x};
 fn main() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
-
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut delay = Delay::new(cp.SYST, clocks);
-
-    let mut leds = Leds::new(dp.GPIOE.split(&mut rcc.ahb));
-
-    let half_period = 50_u16;
+    
+    let mut itm = cp.ITM;
+    iprintln!(&mut itm.stim[0], "Yo bro!!");
 
     loop {
-        for led in 0..8 {
-            leds[led].on();
-            delay.delay_ms(half_period);
-        }
 
-        for led in 0..8 {
-            leds[led].off();
-            delay.delay_ms(half_period);
-        }
     }
 }
